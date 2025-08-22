a701ef8c9c22d2e3d73595076f8125ab0552f6d9
ezzequielcarballo <ezequielcarballo018@gmail.com>
2025-08-22 13:21:52 -0300

    cambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
     <nav>
       <div>
         <a href="#home" className="flex items-center gap-2">
-          <img src="/images/logo.png"></img>
+          <img src="/images/logo.png" alt="logo"></img>
           <p>Velver Pour</p>
         </a>
 
